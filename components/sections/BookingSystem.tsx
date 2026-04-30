08b8483df41887d212d111a1020f354e57dbbf3f
solrac7770 <carlospostigoemanuel@gmail.com>
2026-04-29 22:52:00 -0500

    Equilibrar posición de la sección de reservas (pt-20, scroll-mt-20)

--- components/sections/BookingSystem.tsx
+++ components/sections/BookingSystem.tsx
@@ -143,7 +143,7 @@ export default function BookingSystem() {
   }
 
   return (
-    <section className="pt-4 pb-32 bg-black scroll-mt-0 relative overflow-hidden" id="booking">
+    <section className="pt-20 pb-32 bg-black scroll-mt-20 relative overflow-hidden" id="booking">
 
       {/* Fondo negro geométrico dorado */}
       <div className="absolute inset-0 pointer-events-none">
